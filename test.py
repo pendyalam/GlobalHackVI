70c95717fa4b6af8b0c68ce00b46625231855c01
Jake Smith <smithjacob@slu.edu>
2016-10-20 22:53:34 -0500

    test commit

---
+++ test.py
@@ -0,0 +1,20 @@
+# This is a test file to make sure everyone knows how to use github. 
+# Please do the following:
+
+# 1) Fork and store this file somewhere on your local machine.
+# 2) On the commented line(s) below, put your initials
+
+#
+#
+#
+#
+#
+
+# 3) Use the 'git add' and 'git commit' commands to add your new file 
+#    to your fork.
+# 4) Merge request. Once you do this, I will be able to see the merge 
+#    requests come in and I'll be able to constantly update master.
+# 
+# If you guys can learn how to do all of this using the Git Shell, bonus
+# points for you. If you have any questions, feel free to contact me. 
+    
